1316fd7332dbc4ebb322704740bcacd8dab9f940
T20A02 <70958837+T20A026@users.noreply.github.com>
2022-02-24 22:38:30 -0500

    Update web2.sh

--- web2.sh
+++ web2.sh
@@ -3,5 +3,5 @@ echo "enter username:"
 read x
 echo "enter cert IP"
 read y
-echo openssl req -newkey rsa:2048 -keyout websrv.key -out websrv.csr
-echo scp websrv.csr $x@$y:/tmp
+openssl req -newkey rsa:2048 -keyout websrv.key -out websrv.csr
+scp websrv.csr $x@$y:/tmp
