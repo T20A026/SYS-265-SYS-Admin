9bcbcb0bf0560914efffbc11884d190495fe426b
T20A02 <70958837+T20A026@users.noreply.github.com>
2022-02-24 23:21:39 -0500

    Delete web2.sh

--- web2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "enter username:"
-read x
-echo "enter cert IP"
-read y
-openssl req -newkey rsa:2048 -keyout websrv.key -out websrv.csr
-scp websrv.csr $x@$y:/tmp
-echo scp websrv.csr $x@$y:/tmp
