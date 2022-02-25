7bb3d80c50ddc20eb163b840a3eae198ecf9df8b
T20A02 <70958837+T20A026@users.noreply.github.com>
2022-02-24 22:39:00 -0500

    Update web3.sh

--- web3.sh
+++ web3.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo websrv.crt -> /etc/pki/tls/certs
-echo websrv.key -> /etc/pki/tls/private
-echo yum -y install mod_ssl
-echo vi /etc/httpd/conf.d/ssl.conf
-echo firewall-cmd --add-port=443/tcp --permanent
-echo firewall-cmd --reload
-echo systemctl restart httpd
+websrv.crt -> /etc/pki/tls/certs
+websrv.key -> /etc/pki/tls/private
+yum -y install mod_ssl
+vi /etc/httpd/conf.d/ssl.conf
+firewall-cmd --add-port=443/tcp --permanent
+firewall-cmd --reload
+systemctl restart httpd
