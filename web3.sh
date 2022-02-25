fe6a87ecd5bbaaa90149708f0b87831403586bff
T20A02 <70958837+T20A026@users.noreply.github.com>
2022-02-24 23:21:44 -0500

    Delete web3.sh

--- web3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-websrv.crt -> /etc/pki/tls/certs
-websrv.key -> /etc/pki/tls/private
-yum -y install mod_ssl
-vi /etc/httpd/conf.d/ssl.conf
-firewall-cmd --add-port=443/tcp --permanent
-firewall-cmd --reload
-systemctl restart httpd
